a6e2df4a9c450f875e1c36860b96302aff247d39
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2023-08-10 21:39:00 +0900

    feat: 환경설정

---
+++ src/main/java/com/example/groweam/GroweamApplication.java
@@ -0,0 +1,13 @@
+package com.example.groweam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroweamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroweamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/groweam/GroweamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groweam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroweamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
